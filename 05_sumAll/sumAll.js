1b2a2a5437958d51c66fbb9c72279c13ed01b3ba
BadAdviceBison <139302668+BadAdviceBison@users.noreply.github.com>
2023-08-11 01:09:15 -0700

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isFinite(a) || !Number.isFinite(b)) { //Could have used (typeof a !== 'number' || typeof b !== 'number') but would then have to check for NaN. This way confirms that the result is not NaN and that it's of the number type.
+        return "ERROR"
+    } 
+    if (a < 0 || b < 0) {
+        return "ERROR"
+    }
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    let sumTotal = 0;
+    for (let i = a; i <= b; i++) {
+        sumTotal += i
+    }
+return sumTotal
 
 };
 
